b4d3caf5287ee17b2cde5d7231076d9987581a00
Fabiano <fabianogoes@gmail.com>
2021-06-28 22:43:25 -0300

    first commit

---
+++ src/main/kotlin/com/eprogramar/person/Application.kt
@@ -0,0 +1,23 @@
+package com.eprogramar.person
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
+}
+
+@RestController
+@RequestMapping("/")
+class Welcome {
+
+    @GetMapping
+    fun get() = "Welcome to Person Service!"
+
+}
