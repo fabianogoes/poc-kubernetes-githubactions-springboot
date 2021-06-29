5a05d795a0fd67d8fa8e7c98b5a09ec9ab02e59f
Fabiano <fabianogoes@gmail.com>
2021-06-29 20:38:29 -0300

    MongoDb configuration

--- src/main/kotlin/com/eprogramar/person/Application.kt
+++ src/main/kotlin/com/eprogramar/person/Application.kt
@@ -1,7 +1,12 @@
 package com.eprogramar.person
 
+import org.bson.types.ObjectId
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -15,9 +20,31 @@ fun main(args: Array<String>) {
 
 @RestController
 @RequestMapping("/")
-class Welcome {
+class Welcome(private val repository: PersonRepository) {
+
+    init {
+        listOf(
+            Person(name = "Fabiano"),
+            Person(name = "Dulci"),
+            Person(name = "Leticia"),
+            Person(name = "Davi"),
+        ).forEach(repository::save)
+        repository.findAll().forEach(::println)
+    }
 
     @GetMapping
     fun get() = "Welcome to Person Service!"
 
+    @GetMapping("/persons")
+    fun getPersons() = ResponseEntity.ok(repository.findAll())
+
 }
+
+@Document(collection = "persons")
+data class Person(
+    @Id
+    var id: ObjectId = ObjectId.get(),
+    var name: String? = null
+)
+
+interface PersonRepository : MongoRepository<Person, String>
